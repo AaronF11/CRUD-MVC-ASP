717bd5bd9711d73564c7ac7dfd7af6f80acdb570
AaronF11 <aaron.flores7908@alumnos.udg.mx>
2024-03-18 12:39:18 -0600

    (#4) Update: Make ContactModel requirements essential

--- Models/ContactModel.cs
+++ Models/ContactModel.cs
@@ -1,13 +1,18 @@
-﻿namespace CRUD_MVC_ASP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_MVC_ASP.Models
 {
     public class ContactModel
     {
         public int Id { get; set; }
 
+        [Required]
         public string? Name { get; set; }
 
+        [Required]
         public string? Phone { get; set; }
 
+        [Required]
         public string? Email { get; set; }
     }
 }
