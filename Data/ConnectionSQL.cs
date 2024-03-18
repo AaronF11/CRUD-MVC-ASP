42507fe1e968e3d71be78ade2f1299fb65279b74
AaronF11 <aaron.flores7908@alumnos.udg.mx>
2024-03-17 19:32:53 -0600

    (#1) Create the connection string and ConnectionSQL class

---
+++ Data/ConnectionSQL.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+
+namespace CRUD_MVC_ASP.Data
+{
+    public class ConnectionSQL
+    {
+        private string _connectionString = string.Empty;
+
+        public ConnectionSQL()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            _connectionString = builder.GetConnectionString("Connection");
+        }
+
+        public string GetConnectionString()
+        {
+            return _connectionString;
+        }
+    }
+}
