b264be1e28677cc9c899d96257bd9060fad0f09c
AaronF11 <aaron.flores7908@alumnos.udg.mx>
2024-03-18 12:40:34 -0600

    (#5) Add: MainController

---
+++ Controllers/MainController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUD_MVC_ASP.Data;
+using CRUD_MVC_ASP.Models;
+
+namespace CRUD_MVC_ASP.Controllers
+{
+    public class MainController : Controller
+    {
+
+        DataContact _dataContact = new DataContact();
+
+        /// <summary>
+        /// Show a list of contacts.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult List()
+        {
+            var List = _dataContact.GetListOfContacts();
+            return View(List);
+        }
+
+        /// <summary>
+        /// Return the Save view.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Save()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// receives an object and saves it to the database.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Save(ContactModel Contact)
+        {
+            var answer = _dataContact.CreateContact(Contact);
+
+            if (answer)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
